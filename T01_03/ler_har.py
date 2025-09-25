a6aa6e8dc280296ac82f6ec94d9ddc56b5d2b72e
DavidOteros <a24davidos@iessanclemente.net>
2025-09-25 19:36:04 +0200

    guardado

--- T01_03/ler_har.py
+++ T01_03/ler_har.py
@@ -1,21 +1,99 @@
-import json
+#!/usr/bin/env
 
-"""
-Función que lee un ficheiro HAR
-"""
+import json
+from urllib.parse import urlparse
+
+
 def ler_har(path):
+    """Función que lee un path
+
+    Args:
+        path (string): O path do string
+
+    Returns:
+        list: Devolve unha lista de datos
+    """
     with open(path, 'r', encoding='utf-8') as ficheiro:
         datos = json.load(ficheiro)
     return datos
 
 
-if __name__ == "__main__":
-    # Poñemos a ruta completa do ficheiro HAR
-    ruta = "ficheiro.har"
+def filtrar_json_entries(har_datos):
+    """Función que se encarga de filtrar los mime "application/json"
+
+    Args:
+        har_datos (dict): Pasamoslle un diccionario cos datos
+
+    Returns:
+        list: Devólvese un array cos datos xa filtrados
+    """
+    # Lista donde guardamos el json
+    json_entries = []
     
-    try:
-        contido = ler_har(ruta)
-        print("Ficheiro HAR cargado correctamente!")
-        print("Claves principais do HAR:", contido.keys())
-    except Exception as e:
-        print(f"Erro ao ler o ficheiro HAR: {e}")
+    # Recorremos cada entrada
+    for entry in har_datos.get('log', {}).get('entries', []):
+        # Accedemos a 'response' de forma segura (diccionario vacío si no existe)
+        response = entry.get("response", {})
+        # Accedemos a "content" de forma segura 
+        content = response.get("content", {})
+        # Accedemos a mimetype de forma segura, en caso de no estar devolvemos texto vacio 
+        mime_type = content.get("mimeType", "")
+        
+        
+        if mime_type == "application/json; charset=utf-8":
+            json_entries.append({
+                "url": entry.get('request', {}).get('url', ''),   # URL de la petición
+                "status": response.get('status', ''),             # Código HTTP
+                "content": content.get('text', '')               # Contenido JSON
+            })
+
+    return json_entries
+
+
+def filtrar_dominios(har_datos):
+    
+    if(not(har_datos is type(list))):
+        raise ValueError
+    
+    # Inicializo a lista de dominio
+    dominios = []
+    # Recorremos a lista 
+    for entry in har_datos:
+        #Collemos cada url
+        url = entry.get("url", "")
+        #Compruebo que la url no esté vacía 
+        if not url:
+            continue
+        
+        #Compruebo que la url tenga un formato válido 
+        parsed = urlparse(url)
+        
+        #Compruebo que forme parte el protoco de la url, 
+        if not parsed.scheme or not parsed.hostname:
+            continue
+        #Extraemos el dominio y lso añadimos a la lista si no está repetido 
+        dominio = parsed.hostname
+        
+        if dominio not in dominios:
+            dominios.append(dominio)
+            
+    return dominios
+        
+        
+
+# Poñemos a ruta completa do ficheiro HAR
+ruta = "datos.har"
+
+# Cargamos los datos 
+try:
+    contido = ler_har(ruta)
+    print("Ficheiro HAR cargado correctamente!")
+    print("Claves principais do HAR:", contido.keys())
+
+except Exception as e:
+    print(f"Erro ao ler o ficheiro HAR: {e}")
+
+#Collemos o json cos tipo mime application/json
+diccionario = filtrar_json_entries(contido)
+
+print(filtrar_dominios(diccionario))
